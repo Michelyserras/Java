033126a153ce23f3f0079c7ce08a4623bc8a1124
Michelyserras <Michelysrrs@gmail.com>
2024-09-21 17:46:26 -0300

    ex3 - feito

--- Lista de exercicios/src/ex3.java
+++ Lista de exercicios/src/ex3.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 public class ex3 {
     
     public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira um valor para A: ");
+        int a = leitor.nextInt();
+        System.out.println("Insira um valor para B: ");
+        int b = leitor.nextInt();
+
+        soma_Multiplica(a, b);
+
 
     }
-    
+
+    public static int soma_Multiplica(int a, int b){
+        int soma = 0;
+        int multiplica = 0;
+        if(a == b){
+            soma = a + b;
+            int c = soma;
+            System.out.println(" a soma de A + B = " + c);
+            return c;    
+        }else{
+            multiplica = a * b;
+            int c = multiplica;
+            System.out.println("a multiplicacao de A * B = " + c);
+            return c;
+
+        }
+    }
 }
